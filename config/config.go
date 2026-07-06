c93cccc3b207b16ca03abe8035085d3f42ff90cb
neyzamaylanies <neyzamayylanies@gmail.com>
2026-07-06 09:33:58 +0700

    fix: improve error handling for .env file loading

--- config/config.go
+++ config/config.go
@@ -19,7 +19,7 @@ type Config struct {
 func Load() Config {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Println("No .env file found, using environment variables")
 	}
 
 	return Config{
